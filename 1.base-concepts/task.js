fe2bc9dc4982a1aea4995d31d73ffae71a17a1fa
Yana Kalugina <yanakaluginayana@gmail.com>
2022-11-04 21:51:15 +0300

    Initial commit

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,13 +1,16 @@
+"use strict"
+
 function solveEquation(a, b, c) {
+  
   let arr;
-  // код для задачи №1 писать здесь
-  return arr; // array
-}
 
-function calculateTotalMortgage(percent, contribution, amount, date) {
-  let totalAmount;
-
-  // код для задачи №2 писать здесь
-
-  return totalAmount;
+  const d = b ** 2 - 4 * a * c;
+  if(d < 0) {
+    arr = [];
+  } else if (d = 0) {
+    arr = [-b / (2 * a)];
+  } else {
+    arr = [(-b + Math.sqrt(d) )/(2*a), (-b - Math.sqrt(d) )/(2*a)];
+  }
+  return arr;
 }
