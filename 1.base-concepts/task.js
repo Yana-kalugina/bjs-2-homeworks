134448719a1da4e2db8dee2a619ceed68b989edd
Yana Kalugina <yanakaluginayana@gmail.com>
2022-11-05 20:11:09 +0300

    Second commit

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -2,15 +2,14 @@
 
 function solveEquation(a, b, c) {
   
-  let arr;
+  let arr = [];
 
-  const d = b ** 2 - 4 * a * c;
-  if(d < 0) {
-    arr = [];
-  } else if (d = 0) {
-    arr = [-b / (2 * a)];
-  } else {
-    arr = [(-b + Math.sqrt(d) )/(2*a), (-b - Math.sqrt(d) )/(2*a)];
+  const discriminant = b ** 2 - 4 * a * c;
+
+  if (discriminant === 0) {
+    arr.push(-b / (2 * a));
+  } else if (discriminant > 0) {
+    arr.push((-b + Math.sqrt(discriminant) )/(2*a), (-b - Math.sqrt(discriminant) )/(2*a));
   }
   return arr;
 }
