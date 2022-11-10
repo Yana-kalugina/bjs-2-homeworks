276893f1ebb274f41eda0617798e93dbeef3c4d5
Yana Kalugina <yanakaluginayana@gmail.com>
2022-11-10 18:24:05 +0300

    Initial commit

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,31 +1,58 @@
 // Задание 1
 function getArrayParams(arr) {
-  let min, max, sum, avg;
-
-  // Ваш код
+  let min = Infinity;
+  let max = -Infinity;
+  let sum = 0;
+  let avg = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > max) {
+      max = arr[i];
+    } else if (arr < min) {
+      min = arr[i];
+    }
+    sum += arr[i]; 
+  } 
+  avg = (sum / arr.length).toFixed(2);
 
   return { min: min, max: max, avg: avg };
 }
 
+
+
 // Задание 2
 function worker(arr) {
-  let sum;
-
-  // Ваш код
-
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+  }
   return sum;
 }
 
 function makeWork(arrOfArr, func) {
-  let max;
-
-  // Ваш кода
-  // for ...
-  
+  let max = - Infinity;
+  for (let i = 0; i < arrOfArr.length; i++) {
+    let sumAnother = func(arrOfArr[i]);
+    if (sumAnother > max) {
+      max = sumAnother;
+    }
+  }
   return max;
 }
 
+
 // Задание 3
 function worker2(arr) {
-  // Ваш код
+  let min = Infinity;
+  let max = -Infinity;
+  let difference;
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > max) {
+      max = arr[i];
+    } else if (arr < min) {
+      min = arr[i];
+    }
+  } 
+  difference = Math.abs(max - min);
+  return difference;
 }
