da9ce6b4b40560810932a4050b1da1b8907cfe28
Yana Kalugina <yanakaluginayana@gmail.com>
2022-11-14 19:31:59 +0300

    Second commit

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,29 +1,14 @@
 // Задание 1
-function checkElement(element){
-  return element;
-}
-
-
 function compareArrays(arr1, arr2) {
-  arr1.indexOf(checkElement);
-  arr2.indexOf(checkElement);
-
-  let result = arr1.every(function(arr2) {
-    if (arr1 === arr2) {
-      return true;
-    } else {
-      return false;
-    }
-  });
-  return result; 
+  
+  let result = arr1.length === arr2.length && arr1.every((element, index) => element === arr2[index]);
+  return result;
 }
 
 
 // Задание 2
 function advancedFilter(arr) {
 
-  let resultArr = arr.filter(element => element > 0).filter(element => element % 3 === 0).map(function(element) {
-    return element * 10;
-  });
+  let resultArr = arr.filter(element => element > 0).filter(element => element % 3 === 0).map(element => element * 10);
   return resultArr; 
 }
